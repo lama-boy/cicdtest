b39fc8fae863577bfbbfc3743bbe1db5583a66c6
lama-boy <118721780+lama-boy@users.noreply.github.com>
2024-07-26 13:31:58 +0900

    Add files via upload

---
+++ cicdTestPrj/src/main/java/com/example/cicdTestPrj/CicdTestPrjApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdTestPrj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTestPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdTestPrjApplication.class, args);
+	}
+
+}
---
+++ cicdTestPrj/src/test/java/com/example/cicdTestPrj/CicdTestPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdTestPrj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTestPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
